7d31f6c40951b61590d54b84a58ac9c4937766ea
Aryan Rai <aryan.rai@lukasz.in>
2026-03-27 13:51:37 +0530

    cors policy

--- server/src/index.ts
+++ server/src/index.ts
@@ -15,7 +15,7 @@ import dashboardRoutes from "./routes/dashboard";
 const app = express();
 
 // Middleware
-app.use(cors({ origin: env.CLIENT_URL, credentials: true }));
+app.use(cors({ origin: true, credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 app.use(passport.initialize());
